91dad9645762381368ffbfe39b1ded7b8bea355d
Niky Morgan <nikycode@gmail.com>
2020-03-22 18:35:27 -0400

    rebase alias

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -7,11 +7,11 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 
 # LS
-alias l='ls -lah'
+alias l="ls -lah"
 
 # db
-alias dbmtest='rake db:migrate RAILS_ENV=test'
-alias dbmdev='rake db:migrate RAILS_ENV=development'
+alias dbmtest="rake db:migrate RAILS_ENV=test"
+alias dbmdev="rake db:migrate RAILS_ENV=development"
 
 # Git
 alias gco="git checkout"
@@ -27,32 +27,25 @@ alias gca="git commit -a"
 alias gcam="git commit -am"
 alias gba="git branch -a"
 alias gbv="git branch -v"
-alias gbdall="git branch | grep -v 'master' | xargs git branch -D"
+alias gbdall="git branch | grep -v "master" | xargs git branch -D"
 alias gcm="git checkout master"
 alias grm="git rebase master"
 alias gop="git open"
-
-# Jekyll
-alias js='jekyll serve'
+alias grc="git rebase --continue"
 
 # Rspec
 alias rff="rspec --fail-fast"
-
-# Hub
-eval "$(hub alias -s)"
-alias hubpr="hub pull-request -o"
-alias hubb="hub browse"
 
 # Python
 alias python=/usr/local/bin/python3
 alias pip=/Library/Frameworks/Python.framework/Versions/3.8/bin/pip3
 
 # Rails
-alias rs='rails s'
-alias rc='rails c'
-alias rcs='rails c --sandbox'
+alias rs="rails s"
+alias rc="rails c"
+alias rcs="rails c --sandbox"
 
 # Finder - Show / Unshow Hidden Files
-alias reveal='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias rehide='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias reveal="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
+alias rehide="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
 
