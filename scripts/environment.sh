c49c88d169d13972e81eec5d36a43c21f36e40e6
nikymorg <20468684+nikymorg@users.noreply.github.com>
2023-03-28 12:43:58 -0700

    remove PATH exports

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -14,33 +14,33 @@ export EDITOR="vim"
 
 # Library Paths
 # =====================
-export USR_PATHS="/usr/local:/usr/local/bin:/usr/local/sbin:/usr/bin"
-
-export PATH="$USR_PATHS:$PATH"
-
-# Postgres
-export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
-
-# SQlite
-export PATH="/usr/local/opt/sqlite/bin:$PATH"
-
-# OpenSSL
-export PATH="/usr/local/opt/openssl/bin:$PATH"
-
-# Mongo
-export MONGO_PATH=/usr/local/mongodb
-export PATH=$PATH:$MONGO_PATH/bin
-
-# NVM - Node Version Manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Setting PATH for Python 3.8
-PATH="/Library/Frameworks/Python.framework/Versions/3.8/bin:${PATH}"
-
-# RVM - Ruby Version Manager
-export PATH="$PATH:$HOME/.rvm/bin"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
-export PATH
+#export USR_PATHS="/usr/local:/usr/local/bin:/usr/local/sbin:/usr/bin"
+#
+#export PATH="$USR_PATHS:$PATH"
+#
+## Postgres
+#export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
+#
+## SQlite
+#export PATH="/usr/local/opt/sqlite/bin:$PATH"
+#
+## OpenSSL
+#export PATH="/usr/local/opt/openssl/bin:$PATH"
+#
+## Mongo
+#export MONGO_PATH=/usr/local/mongodb
+#export PATH=$PATH:$MONGO_PATH/bin
+#
+## NVM - Node Version Manager
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#
+## Setting PATH for Python 3.8
+#PATH="/Library/Frameworks/Python.framework/Versions/3.8/bin:${PATH}"
+#
+## RVM - Ruby Version Manager
+#export PATH="$PATH:$HOME/.rvm/bin"
+#[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+#
+#export PATH
