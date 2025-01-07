a90ea8dc90bd7118d006611d3e323d211047d5ec
nikymorg <20468684+nikymorg@users.noreply.github.com>
2025-01-07 10:32:17 -0800

    independent nvim setup

---
+++ config/nvim/init.lua
@@ -0,0 +1,32 @@
+-- from https://github.com/nvim-lua/kickstart.nvim
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'dense-analysis/ale', -- syntax checking
+  'github/copilot.vim', -- AI code suggestions
+  'junegunn/fzf', -- fuzzyfinder
+  'tpope/vim-vinegar', -- filenav
+})
+
